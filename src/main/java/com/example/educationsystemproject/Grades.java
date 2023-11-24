08ec420beae8354591e8563022e6df268b6e5ddc
Justin Kawaja <jkawaja909@gmail.com>
2023-11-24 19:52:42 -0330

    added new attributes to Grades + getters/setters

---
+++ src/main/java/com/example/educationsystemproject/Grades.java
@@ -0,0 +1,48 @@
+package com.example.educationsystemproject;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Grades {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Integer gid;
+    private Integer studentID;
+    private Integer courseID;
+    private String grade;
+
+    public Integer getGid() {
+        return gid;
+    }
+
+    public void setGid(Integer gid) {
+        this.gid = gid;
+    }
+
+    public Integer getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(Integer studentID) {
+        this.studentID = studentID;
+    }
+
+    public Integer getCourseID() {
+        return courseID;
+    }
+
+    public void setCourseID(Integer courseID) {
+        this.courseID = courseID;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+}
