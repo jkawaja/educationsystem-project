849914808a04a7742971199c0606bf9f9099d55d
Justin Kawaja <jkawaja909@gmail.com>
2023-11-24 19:54:57 -0330

    added new attributes to Enrollment + getters/setters

---
+++ src/main/java/com/example/educationsystemproject/Enrollment.java
@@ -0,0 +1,39 @@
+package com.example.educationsystemproject;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Enrollment {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Integer eid;
+    private Integer courseID;
+    private Integer studentID;
+
+    public Integer getEid() {
+        return eid;
+    }
+
+    public void setEid(Integer eid) {
+        this.eid = eid;
+    }
+
+    public Integer getCourseID() {
+        return courseID;
+    }
+
+    public void setCourseID(Integer courseID) {
+        this.courseID = courseID;
+    }
+
+    public Integer getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(Integer studentID) {
+        this.studentID = studentID;
+    }
+}
