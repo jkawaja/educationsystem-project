e3730191875bb8e3673f4e158b84c73ad4202267
Justin Kawaja <jkawaja909@gmail.com>
2023-11-24 20:22:35 -0330

    added new repository interfaces for each of the tables

---
+++ src/main/java/com/example/educationsystemproject/EnrollmentRepository.java
@@ -0,0 +1,6 @@
+package com.example.educationsystemproject;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface EnrollmentRepository extends CrudRepository<Enrollment, Integer> {
+}
---
+++ src/main/java/com/example/educationsystemproject/GradesRepository.java
@@ -0,0 +1,6 @@
+package com.example.educationsystemproject;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface GradesRepository extends CrudRepository<Grades, Integer> {
+}
