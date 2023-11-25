c5289d6f6517a7ae39412d759d7ad5f82e54ef01
Justin Kawaja <jkawaja909@gmail.com>
2023-11-25 16:56:57 -0330

    findGradesByGID method added to repository

--- src/main/java/com/example/educationsystemproject/GradesRepository.java
+++ src/main/java/com/example/educationsystemproject/GradesRepository.java
@@ -3,4 +3,5 @@ package com.example.educationsystemproject;
 import org.springframework.data.repository.CrudRepository;
 
 public interface GradesRepository extends CrudRepository<Grades, Integer> {
+    Grades findGradesBygid(Integer gid);
 }
