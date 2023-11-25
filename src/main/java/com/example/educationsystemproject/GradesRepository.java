7136559c2e815ee2e9af684e53270141e611eb9c
Justin Kawaja <jkawaja909@gmail.com>
2023-11-25 19:20:55 -0330

    added findGradesByCourseID and findGradesByStudentID

--- src/main/java/com/example/educationsystemproject/GradesRepository.java
+++ src/main/java/com/example/educationsystemproject/GradesRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface GradesRepository extends CrudRepository<Grades, Integer> {
     Grades findGradesBygid(Integer gid);
+    Iterable<Grades> findGradesByCourseID(Integer courseID);
+    Iterable<Grades> findGradesByStudentID(Integer studentID);
 }
