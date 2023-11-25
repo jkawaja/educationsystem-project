d8695aeb9368b36c696c170042c68ebec2fb05bb
Justin Kawaja <jkawaja909@gmail.com>
2023-11-25 19:14:39 -0330

    added findProgramsBypid (Integer pid)

--- src/main/java/com/example/educationsystemproject/ProgramsRepository.java
+++ src/main/java/com/example/educationsystemproject/ProgramsRepository.java
@@ -2,5 +2,8 @@ package com.example.educationsystemproject;
 
 import org.springframework.data.repository.CrudRepository;
 
+
+
 public interface ProgramsRepository extends CrudRepository<Programs, Integer> {
+    Programs findProgramsBypid(Integer pid);
 }
