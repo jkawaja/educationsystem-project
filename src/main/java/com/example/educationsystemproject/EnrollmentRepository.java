638976a54bc2a6864745d53341e3b871d397a1b7
Justin Kawaja <jkawaja909@gmail.com>
2023-11-25 19:11:14 -0330

    added find by courseID/StudentID to repository

--- src/main/java/com/example/educationsystemproject/EnrollmentRepository.java
+++ src/main/java/com/example/educationsystemproject/EnrollmentRepository.java
@@ -3,4 +3,8 @@ package com.example.educationsystemproject;
 import org.springframework.data.repository.CrudRepository;
 
 public interface EnrollmentRepository extends CrudRepository<Enrollment, Integer> {
+    Enrollment findEnrollmentByeid(Integer eid);
+    Iterable<Enrollment> findEnrollmentByCourseID(Integer courseID);
+    Iterable<Enrollment> findEnrollmentByStudentID(Integer studentID);
+
 }
